46c625e3e0ef100237b7d4155cd79c35ad0106e2
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-05-19 19:56:11 +0200

    Lists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { searchCompanies } from './api';
 function App() {
   const [search, setSearch] = useState<string>("");
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
+  const [serverError, setServerError] = useState<string | null>(null);
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
@@ -16,7 +16,7 @@ function App() {
     const onClick = async (e: SyntheticEvent) => {
       const result = await searchCompanies(search);
       if(typeof result === "string") {
-
+        setServerError(result);
       } else if (Array.isArray(result.data)) {
         setSearchResult(result.data);
       }
@@ -25,8 +25,8 @@ function App() {
   return (
     <div className='App'>
       <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      {serverError && <h1>{serverError}</h1>}
-      <CardList />
+      <CardList searchResults={searchResult}/>
+      {serverError && <div>Unable to connect to API</div>}
     </div>
   );
 }
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Card.css";
+import { CompanySearch } from "../../company";
 
 interface Props {
-    companyName: string;
-    ticker: string;
-    price: number;
+    id: string;
+    searchResult : CompanySearch;
 }
 
-const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element => {
     return (
         <div className="card">
+            <img alt="company logo" />
             <div className="details">
-                <h2>{companyName} {ticker}</h2>
-                <p>${price}</p>
+                <h2>{searchResult.name} {searchResult.symbol}</h2>
+                <p>${searchResult.currency}</p>
             </div>
-            <p className="infon">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, soluta!</p>    
+            <p className="info">
+                {searchResult.exchangeShortName} - {searchResult.stockExchange}    
+            </p>    
         </div>
         
     )
